20a81d99268cba68ed8b5bfa52b94c2794460533
Masum <engmasum90@gmail.com>
2016-08-19 18:40:09 +0800

    no message

---
+++ README.md
@@ -0,0 +1,22 @@
+# webbycms - with Foundation 6
+
+1) Clone/Download, then select Bootraps4 branch to start develop your PHP project.
+
+2) Setup your MAMP Pro with localhost
+
+3) Access your localhost, you should see a Foundation Hello World Page
+
+4) Enjoy Coding!
+
+Developer Skill: PHP, mySQL Improved, HTML and CSS Bootstrap, Javascripts
+
+Clone and start build
+Recommened Tools:
+- SourceTree
+- Sublime Text 3
+- MAMP Pro for local development
+- SequelPro (Mac OS), MYSQL WorkBench (Windows)
+
+#Others
+Bootstraps v4
+https://github.com/phototix/webbycms/tree/Bootstrap4/
---
+++ includes/htmlstart.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>WebbyCMS | Bootstraps</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Webby Custom CSS -->
+  <link rel="stylesheet" href="/assets/css/custom.css">
+  <!-- Webby Custom Javascripts -->
+  <script src="/assets/js/custom.js"></script>
+
+  <!-- Foundations 6 -->
+  <link rel="stylesheet" href="/assets/css/foundation.css">
+  <link rel="stylesheet" href="/assets/css/app.css">
+</head>
